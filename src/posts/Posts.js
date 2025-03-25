b873e22d8f25763b86a78bce248df97eb12f60e7
SirFK99 <hutchings.kendall@gmail.com>
2025-03-24 23:08:33 -0300

    image import

--- src/posts/Posts.js
+++ src/posts/Posts.js
@@ -1,12 +1,12 @@
 import farts from '../posts/sample.md';
 import ass from '../assets/img/post_1.png'
-
+import grass from '../assets/img/josh.png'
 const Posts = [
     {
         title: "Why am I doing this?",
         date: "March 20, 2025",
         description: "I want to show you my abilities in learning how to build websites and how I accomplished that.",
-        image: ass,
+        image: grass,
         content: farts,
         route: "kennyg"
     },
