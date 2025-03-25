64502ae242ffce67f1862d6065707e47b5e7ed67
SirFK99 <hutchings.kendall@gmail.com>
2025-03-24 22:50:24 -0300

    conent change

--- src/posts/Posts.js
+++ src/posts/Posts.js
@@ -12,17 +12,17 @@ const Posts = [
         route: "kennyg"
     },
     {
-        title: "Why am I doing this?",
-        date: "March 20, 2025",
-        description: "I want to show you my abilities in learning how to build websites and how I accomplished that.",
+        title: "Down the Rabbit Hole",
+        date: "March 21, 2025",
+        description: "As James makes his way through Silent Hill he finds himself digging deeper and deeper into what really happened to his wife. he stares into the empty void not knowing what he'll face but he goes anyways. James would rather face the truth than to continue living the way he has.",
         image: grass,
         content: farts,
         route: "kennyg"
     },
     {
-        title: "Why am I doing this?",
-        date: "March 20, 2025",
-        description: "I want to show you my abilities in learning how to build websites and how I accomplished that.",
+        title: "Meow?",
+        date: "March 22, 2025",
+        description: "I really just wanted to show one of my favourite photos I captured from my playthrough of Final Fantasy VII: Rebirth.",
         image: ass,
         content: farts,
         route: "kennyg"
